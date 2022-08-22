0af39dfe926758c78e142156a88a279f329a537f
flaviobartolo <flavio.bartolo2@gmail.com>
2022-08-22 13:46:10 +0100

    getting route params & adding custom buttons component

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 import PlaceList from '../components/PlaceList'
 
@@ -30,7 +31,12 @@ const DUMMY_PLACES = [
 ]
 
 const UserPlaces = () => {
-  return <PlaceList items={DUMMY_PLACES} />
+
+  const params = useParams()
+  const userId = params.userId
+  const loadedPlaces = DUMMY_PLACES.filter((place) => place.creator === userId)
+
+  return <PlaceList items={loadedPlaces} />
 }
 
 export default UserPlaces
--- src/shared/components/Navigation/SideDrawer.js
+++ src/shared/components/Navigation/SideDrawer.js
@@ -14,7 +14,7 @@ const SideDrawer = (props) => {
       mountOnEnter 
       unmountOnExit
     >
-      <aside className='side-drawer' onClick={props.onClick} >{props.children}</aside>
+      <aside className='side-drawer' onClick={props.onClick}>{props.children}</aside>
     </CSSTransition>
   )
 
