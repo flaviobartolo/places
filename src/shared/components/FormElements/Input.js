34cd36effbe07db66f5313b5305db3f4b2530a50
flaviobartolo <flavio.bartolo2@gmail.com>
2022-08-23 00:06:58 +0100

    Managing state in the input component

--- src/shared/components/FormElements/Input.js
+++ src/shared/components/FormElements/Input.js
@@ -1,18 +1,53 @@
-import React from 'react'
+import React, { isValidElement, useReducer } from 'react'
 
 import './Input.css'
 
+const inputReducer = (state, action) => {
+  switch (action.type) {
+    case 'CHANGE':
+      return {
+        ...state,
+        value: action.val,
+        isValid: true
+      }
+    default:
+      return state;
+  }
+}
+
 const Input = (props) => {
   
-  const element = props.element === 'input' ? (
-      <input id={props.id} type={props.type} placeholder={props.placeholder} />
-    ) : (
-      <textarea id={props.id} rows={props.rows || 3} placeholder={props.placeholder} />
-    )
+  const [inputState, dispatch] = useReducer(inputReducer, {value: '', isValid: false})
   
-  return <div className={`form-control`}>
+  const changeHandler = (e) => {
+    dispatch({
+      type: 'CHANGE',
+      val: e.target.value
+    })
+  }
+
+  const element = props.element === 'input' ? (
+      <input 
+        id={props.id} 
+        type={props.type} 
+        placeholder={props.placeholder} 
+        onChange={changeHandler}
+        value={inputState.value}
+      />
+    ) : (
+      <textarea 
+        id={props.id} 
+        rows={props.rows || 3} 
+        placeholder={props.placeholder} 
+        onChange={changeHandler}
+        value={inputState.value}
+      />
+    )
+
+  return <div className={`form-control ${!inputState.isValid && 'form-control--invalid'}`}>
     <label htmlFor='props.id'>{props.label}</label>
     {element}
+    {!inputState.isValid && <p>{props.errorText}</p>}
   </div>
 }
 
