80a69c513a4d45ca87745a8b95ab2fca39ca01ff
flaviobartolo <flavio.bartolo2@gmail.com>
2022-08-23 23:46:26 +0100

    Delete place modal and buttons

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -9,9 +9,19 @@ import './PlaceItem.css'
 
 const PlaceItem = (props) => {
   const [showMap, setShowMap] = useState(false)
+  const [showDeleteModal, setShowDeleteModal] = useState(false)
 
   const toggleShowMapHandler = () => {
     showMap ? setShowMap(false) : setShowMap(true)
+  }
+
+  const toggleShowDeleteModalHandler = () => {
+    showDeleteModal ? setShowDeleteModal(false) : setShowDeleteModal(true)
+  }
+
+  const confirmDeleteHandler = () => {
+    console.log('DUMMY MSG - DELETING THIS PLACE')
+    setShowDeleteModal(false)
   }
 
   return <>
@@ -27,6 +37,20 @@ const PlaceItem = (props) => {
           <Map center={props.coordinates} zoom={16} />
         </div>
       </Modal>
+      <Modal 
+        header='Are you sure?' 
+        footerClass='place-item__modal-actions' 
+        footer={
+          <>
+            <Button inverse onClick={toggleShowDeleteModalHandler}>CANCEL</Button>
+            <Button danger onClick={confirmDeleteHandler} >DELETE</Button>
+          </>
+        }
+        show={showDeleteModal}
+        onCancel={toggleShowDeleteModalHandler}
+      >
+        <p>Do you want to proceed and delete this place? please note it can't be undone after.</p>
+      </Modal>
       <li className='place-item'>
         <Card className='place-item__content'>
           <div className="place-item__image">
@@ -40,7 +64,7 @@ const PlaceItem = (props) => {
           <div className="place-item__actions">
             <Button inverse onClick={toggleShowMapHandler}>VIEW ON MAP</Button>
             <Button to={`/places/${props.id}`}>EDIT</Button>
-            <Button danger>DELETE</Button>
+            <Button danger onClick={toggleShowDeleteModalHandler}>DELETE</Button>
           </div>
         </Card>
       </li>
