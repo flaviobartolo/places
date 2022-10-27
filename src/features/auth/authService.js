3803f5bf1862565b45b6b935f46948521e8effe6
flaviobartolo <flavio.bartolo2@gmail.com>
2022-10-27 16:56:35 +0100

    removing .env

--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -1,9 +1,11 @@
 import userAxios from '../../axios-instances/user-instance'
 
-
 const createUser = async (data) => {
   const response = await userAxios.post('signup', data)
   if (response.data.token) {
+    const expDate = expirationDate(1000 * 60 * 60)
+    console.log(expDate)
+    console.log(JSON.stringify(response.data, {'a': 'b'}))
     localStorage.setItem('user', JSON.stringify(response.data))
   }
   return response.data
@@ -13,6 +15,9 @@ const loginUser = async (data) => {
   const response = await userAxios.post('login', data)
   console.log(response)
   if (response.data.token) {
+    const expDate = expirationDate(1000 * 60 * 60)
+    console.log(expDate)
+    console.log(JSON.stringify(response.data, {'a': 'b'}))
     localStorage.setItem('user', JSON.stringify(response.data))
   }
   return response.data
@@ -30,3 +35,10 @@ const authService = {
 }
 
 export default authService
+
+
+// helper function for expiration date
+
+const expirationDate = (ms) => {
+  return (new Date(new Date().getTime() + ms)).toISOString()
+}
